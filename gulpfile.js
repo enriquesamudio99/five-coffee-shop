afc12012ec03d765b13a77d4c9063ed89717ae6d
Enrique <enriquesamudio1999@gmail.com>
2021-09-15 18:00:12 -0400

    Project started with sass compiler

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+const { src, dest, watch, series } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+
+function compileCSS(done) {
+    src("src/scss/app.scss")
+        .pipe( sass({
+            outputStyle: "expanded"
+        }) )
+        .pipe(postcss([autoprefixer()]))
+        .pipe( dest("build/css"));
+    done();
+}
+
+function dev() {
+    watch("src/scss/app.scss", compileCSS);
+}
+
+exports.compileCSS = compileCSS;
+exports.dev = dev; 
+exports.default = series( compileCSS, dev );
