cb6480c79ea56c8ec319a770ff4e72f979d46cc3
Enrique <enriquesamudio1999@gmail.com>
2021-09-15 19:45:59 -0400

    Imagemin add

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,12 @@
 const { src, dest, watch, series } = require("gulp");
+
+// CSS, SASS
 const sass = require("gulp-sass")(require("sass"));
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
+
+// IMG
+const imagemin = require("gulp-imagemin");
 
 function compileCSS(done) {
     src("src/scss/app.scss")
@@ -13,10 +18,22 @@ function compileCSS(done) {
     done();
 }
 
+function images(done) {
+    src("src/img/**/*")
+        .pipe(imagemin({
+            optimizationLevel: 3
+        }))
+        .pipe(dest("build/img"));
+
+    done();
+}
+
 function dev() {
     watch("src/scss/**/*.scss", compileCSS);
+    watch("src/img/**/*", images);
 }
 
 exports.compileCSS = compileCSS;
 exports.dev = dev; 
-exports.default = series( compileCSS, dev );
+exports.images = images; 
+exports.default = series( images, compileCSS, dev );
